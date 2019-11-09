4241839d97131ea71fa0a8c120f18c2404769f1f
Katy ORourke <katyshayfox@gmail.com>
2019-11-09 16:07:54 -0700

    Specified the response type on each controller endpoint so that it is clear in the Swagger documentation what is expected to be returned

--- src/main/java/com/katy/groceries/controller/GroceryItemController.java
+++ src/main/java/com/katy/groceries/controller/GroceryItemController.java
@@ -3,6 +3,7 @@ package com.katy.groceries.controller;
 import com.katy.groceries.model.GroceryItem;
 import com.katy.groceries.service.IGroceryItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,13 +17,13 @@ public class GroceryItemController {
     @Autowired
     private IGroceryItemService groceryItemService;
 
-    @GetMapping(value="/groceryItem")
+    @GetMapping(value="/groceryItem", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<GroceryItem> list() {
         List<GroceryItem> groceryItems = groceryItemService.getAll();
         return groceryItems;
     }
 
-    @PostMapping(value="/groceryItem")
+    @PostMapping(value="/groceryItem", produces = MediaType.APPLICATION_JSON_VALUE)
     public GroceryItem create(@RequestBody GroceryItem groceryItem) {
         return groceryItemService.add(groceryItem);
     }
